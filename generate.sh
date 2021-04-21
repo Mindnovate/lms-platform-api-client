7337a4d782d815e226c01233f43cead1b1eea480
= <=>
2021-04-21 15:05:24 +0800

    Generate Typescript client

--- generate.sh
+++ generate.sh
@@ -1,22 +1,29 @@
 #!/bin/bash
 
-rm -rf ./php;
+# rm -rf ./php;
+
+# java -jar swagger-codegen-cli.jar generate \
+#    -i "https://www.starfishlabz.com/api/docs.json?spec_version=3" \
+#    -l php \
+#    -o ./php;
+
+# rm -rf ./javascript;
+
+# java -jar swagger-codegen-cli.jar generate \
+#    -i "https://www.starfishlabz.com/api/docs.json?spec_version=3" \
+#    -l javascript \
+#    -o ./javascript;
+
+# rm -rf ./dart;
+
+# java -jar swagger-codegen-cli.jar generate \
+#    -i "https://www.starfishlabz.com/api/docs.json?spec_version=3" \
+#    -l dart \
+#    -o ./dart;
+
+rm -rf ./typescript-fetch;
 
 java -jar swagger-codegen-cli.jar generate \
    -i "https://www.starfishlabz.com/api/docs.json?spec_version=3" \
-   -l php \
-   -o ./php;
-
-rm -rf ./javascript;
-
-java -jar swagger-codegen-cli.jar generate \
-   -i "https://www.starfishlabz.com/api/docs.json?spec_version=3" \
-   -l javascript \
-   -o ./javascript;
-
-rm -rf ./dart;
-
-java -jar swagger-codegen-cli.jar generate \
-   -i "https://www.starfishlabz.com/api/docs.json?spec_version=3" \
-   -l dart \
-   -o ./dart;
+   -l typescript-fetch \
+   -o ./typescript-fetch;
